6ea37db5175c13e996396fd33526f9dcc182a6ed
Daniel Luca <lucadanielcostin@gmail.com>
2020-04-22 16:19:35 +0300

    Testing stuff.

--- data_types/src/main.rs
+++ data_types/src/main.rs
@@ -1,15 +1,59 @@
-use std::io;
+// use std::io;
+
+// fn parse_number() {
+//     let mut number = String::new();
+
+//     println!("Enter number:");
+
+//     io::stdin()
+//         .read_line(&mut number)
+//         .expect("Failed to read line");
+
+//     let number: u8 = number
+//         .trim()
+//         .parse()
+//         .expect("Could not transform to number.");
+
+//     println!("number = {}", number);
+// }
+
+fn characters() {
+    let c = String::from("z");
+    let emoji = '🚀';
+
+    println!("{} {}", c, emoji);
+}
+
+fn tuples() {
+    let x: (i32, f64, u8) = (500, 1.2, 123);
+
+    let _y = 2;
+
+    println!("{}", x.2);
+}
+
+fn arrays() {
+    const ARR_LENGTH: usize = 500000;
+
+    let a: [i32; ARR_LENGTH] = [1234; ARR_LENGTH];
+
+    println!("{}", a[1]);
+}
 
 fn main() {
-    let mut number = String::new();
-    
-    println!("Enter number:");
+    arrays();
+    tuples();
+    characters();
+    // parse_number();
 
-    io::stdin()
-        .read_line(&mut number)
-        .expect("Failed to read line");
+    let arr = arrays;
 
-    let number: u8 = number.trim().parse().expect("Could not transform to number.");
+    let mut i = 0;
 
-    println!("number = {}", number);
+    loop {
+        i = i + 1;
+        println!("{}", i)
+    }
+
+    arr();
 }
